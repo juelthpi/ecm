d02769ddec5291e0d06057cf93eb8a96b59980d1
juel <juelcse2019@gmail.com>
2024-11-13 00:14:51 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+
+jQuery(document).ready(function ($) {
+ 
+    $(document).on("click", function (event) {
+      const $div = $("#outside-click");
+      const $navbarText = $("#navbarText");
+      if (!$div.is(event.target) && $div.has(event.target).length === 0) {
+        $navbarText.removeClass("show");
+      }
+    });
+    
+
+  // menu bg add
+    $(window).on('scroll', function() {
+      if ($(window).scrollTop() > 300) {
+          $('header').addClass('menu-bg-fixed');
+      } else {
+          $('header').removeClass('menu-bg-fixed');
+      }
+  });
+ 
+  $(document).click(function(e) {
+    var target = e.target;
+    if (!$(target).is('.dropdown-toggle') && !$(target).parents().is('.dropdown-toggle')) {
+      $('.dropdown').hide() ;
+    }
+  });
+
+
+})
+ 
